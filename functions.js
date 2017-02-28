0f469ac8e6dbe4c0e35a2cde8f03f61fa8b8c044
pekarsky.maxim@gmail.com <flufci@gmail.com>
2017-02-27 23:03:30 -0500

    add even more styling and responsiveness

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-var shops = ["Idea Coffee", "Stumptown", "Gregory's Coffee", "Starbucks", "Birch", "A Little Taste"];
+var shops = ["Idea Coffee", "Stumptown", "Gregory's", "Starbucks", "Birch", "A Little Taste"];
 
 function pickCoffeeShop(){
 	return shops[Math.floor(Math.random()*shops.length)];
